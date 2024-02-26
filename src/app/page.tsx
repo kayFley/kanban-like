016ef559398c605a3ba5ded8f4a301a29e5d0cd9
kayFley <vasya.shumilov.2003@gmail.com>
2024-02-26 20:43:55 +0700

    Layout and style delete section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import React, { useState } from 'react'
+import { FaFire } from 'react-icons/fa'
+import { FiTrash } from 'react-icons/fi'
 
 export default function NotionKanban() {
 	return (
@@ -46,6 +48,7 @@ const Board = () => {
 					setCards={setCards}
 				/>
 			</div>
+			<BurnBarrel setCards={setCards} />
 		</div>
 	)
 }
@@ -96,6 +99,23 @@ const DropIndicator = ({ beforeId, column }) => {
 			data-column={column}
 			className='my-0.5 h-0.5 w-full bg-violet-400 opacity-0'
 		/>
+	)
+}
+
+const BurnBarrel = ({ setCards }) => {
+	const [active, setActive] = useState(false)
+
+	return (
+		<div
+			className={`mt-10 grid h-56 w-56 shrink-0 place-content-center rounded border text-3xl 
+			${
+				active
+					? 'border-red-800 bg-red-800/20 text-red-500'
+					: 'border-neutral-500 bg-neutral-500/20 text-neutral-500'
+			}`}
+		>
+			{active ? <FaFire className='animate-bounce' /> : <FiTrash />}
+		</div>
 	)
 }
 
