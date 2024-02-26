4e1cf70aa431780a5eceba1a52f3633d1fce86eb
kayFley <vasya.shumilov.2003@gmail.com>
2024-02-26 22:30:40 +0700

    Add: layout animation, column highlighting indicators

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React, { useState } from 'react'
+import { motion } from 'framer-motion'
 import { FaFire } from 'react-icons/fa'
 import { FiPlus, FiTrash } from 'react-icons/fi'
 
@@ -57,6 +58,99 @@ const Column = ({ title, headingColor, column, cards, setCards }) => {
 		e.dataTransfer.setData('cardId', card.id)
 	}
 
+	const handleDragEnd = e => {
+		const cardId = e.dataTransfer.getData('cardId')
+
+		setActive(false)
+		clearHighlights()
+
+		const indicators = getIndicators()
+		const { element } = getNearestIndicator(e, indicators)
+
+		const before = element.dataset.before || '-1'
+
+		if (before !== cardId) {
+			let copy = [...cards]
+
+			let cardToTransfer = copy.find(c => c.id === cardId)
+			if (!cardToTransfer) return
+			cardToTransfer = { ...cardToTransfer, column }
+
+			copy = copy.filter(c => c.id !== cardId)
+
+			const moveToBack = before === '-1'
+
+			if (moveToBack) {
+				copy.push(cardToTransfer)
+			} else {
+				const insertAtIndex = copy.findIndex(el => el.id === before)
+				if (insertAtIndex === undefined) return
+
+				copy.splice(insertAtIndex, 0, cardToTransfer)
+			}
+
+			setCards(copy)
+		}
+	}
+
+	const handleDragOver = e => {
+		e.preventDefault()
+		highlightIndicator(e)
+
+		setActive(true)
+	}
+
+	const clearHighlights = els => {
+		const indicators = els || getIndicators()
+
+		indicators.forEach(i => {
+			i.style.opacity = '0'
+		})
+	}
+
+	const highlightIndicator = e => {
+		const indicators = getIndicators()
+
+		clearHighlights(indicators)
+
+		const el = getNearestIndicator(e, indicators)
+
+		el.element.style.opacity = '1'
+	}
+
+	const getNearestIndicator = (e, indicators) => {
+		const DISTANCE_OFFSET = 50
+
+		const el = indicators.reduce(
+			(closest, child) => {
+				const box = child.getBoundingClientRect()
+
+				const offset = e.clientY - (box.top + DISTANCE_OFFSET)
+
+				if (offset < 0 && offset > closest.offset) {
+					return { offset: offset, element: child }
+				} else {
+					return closest
+				}
+			},
+			{
+				offset: Number.NEGATIVE_INFINITY,
+				element: indicators[indicators.length - 1],
+			},
+		)
+
+		return el
+	}
+
+	const getIndicators = () => {
+		return Array.from(document.querySelectorAll(`[data-column="${column}"]`))
+	}
+
+	const handleDragLeave = () => {
+		clearHighlights()
+		setActive(false)
+	}
+
 	const filteredCards = cards.filter(c => c.column === column)
 
 	return (
@@ -68,6 +162,9 @@ const Column = ({ title, headingColor, column, cards, setCards }) => {
 				</span>
 			</div>
 			<div
+				onDragOver={handleDragOver}
+				onDragLeave={handleDragLeave}
+				onDrop={handleDragEnd}
 				className={`h-full w-full transition-colors 
 				${active ? 'bg-neutral-800/50' : 'bg-neutral-800/0'}`}
 			>
@@ -85,13 +182,15 @@ const Card = ({ title, id, column, handleDragStart }) => {
 	return (
 		<>
 			<DropIndicator beforeId={id} column={column} />
-			<div
+			<motion.div
+				layout
+				layoutId={id}
 				draggable='true'
 				onDragStart={e => handleDragStart(e, { title, id, column })}
 				className='cursor-grap rounded border border-neutral-700 bg-neutral-800 p-3 active:cursor-grabbing'
 			>
 				<p className='text-sm text-neutral-100'>{title}</p>
-			</div>
+			</motion.div>
 		</>
 	)
 }
@@ -166,7 +265,7 @@ const AddCard = ({ column, setCards }) => {
 	return (
 		<>
 			{adding ? (
-				<form onSubmit={handleSubmit}>
+				<motion.form layout onSubmit={handleSubmit}>
 					<textarea
 						onChange={e => setText(e.target.value)}
 						autoFocus
@@ -190,16 +289,17 @@ const AddCard = ({ column, setCards }) => {
 							<FiPlus />
 						</button>
 					</div>
-				</form>
+				</motion.form>
 			) : (
-				<button
+				<motion.button
+					layout
 					onClick={() => setAdding(true)}
 					className='flex w-full items-center gap-1.5 px-3 py-1.5 text-xs
 					text-neutral-400 transition-colors hover:text-neutral-50'
 				>
 					<span>Add card</span>
 					<FiPlus />
-				</button>
+				</motion.button>
 			)}
 		</>
 	)
